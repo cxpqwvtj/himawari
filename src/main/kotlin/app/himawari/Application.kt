71796381ae1db48ed115f803fd99b16811a8b64f
cxpqwvtj <cxpqwvtj@gmail.com>
2016-10-02 09:00:08 +0900

    アプリケーションエントリーとなるクラスを追加

---
+++ src/main/kotlin/app/himawari/Application.kt
@@ -0,0 +1,22 @@
+package app.himawari
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+/**
+ * Created by masahiro on 2016/010/02.
+ */
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan("app.himawari")
+@SpringBootApplication
+open class Application {
+    companion object {
+        @JvmStatic fun main(args: Array<String>) {
+            SpringApplication.run(Application::class.java, *args)
+        }
+    }
+}
