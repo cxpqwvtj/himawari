57438ed730f8ebb50444b3724ac3aa06be957622
cxpqwvtj <cxpqwvtj@gmail.com>
2016-10-12 07:08:33 +0900

    ルートアドレス用のコントローラ作成

---
+++ src/main/kotlin/app/himawari/controller/RootController.kt
@@ -0,0 +1,21 @@
+package app.himawari.controller
+
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import javax.servlet.http.HttpServletResponse
+
+/**
+ * Created by masahiro on 2016/10/12.
+ */
+@Controller
+@RequestMapping("/")
+open class RootController {
+    @GetMapping(path = arrayOf("", "timber/**"))
+    open fun root(response: HttpServletResponse) {
+        response.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_HTML_VALUE)
+        response.outputStream.use { it.write(this.javaClass.getResource("/static/index.html").readBytes()) }
+    }
+}
