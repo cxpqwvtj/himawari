db575b600484deefb5aa14d747b1fc4bda9cc156
cxpqwvtj <cxpqwvtj@gmail.com>
2016-10-15 07:04:35 +0900

    listのレスポンスオブジェクト構成修正

--- src/main/kotlin/app/himawari/dto/json/TimeCardListResponse.kt
+++ src/main/kotlin/app/himawari/dto/json/TimeCardListResponse.kt
@@ -1,10 +1,12 @@
 package app.himawari.dto.json
 
-import java.util.*
-
 /**
  * Created by masahiro on 2016/10/13.
  */
-data class TimeCardListResponse(var result: List<TimeCard>) {
-    data class TimeCard(var yearMonth: String)
+data class TimeCardListResponse(var result: TimeCardList? = null,
+                                var errorCode: String? = null,
+                                var errorMessage: String? = null) {
+    data class TimeCardList(val timecards: List<TimeCard> = listOf()) {
+        data class TimeCard(var yearMonth: String)
+    }
 }
