55add4089657603ba30d116a62d015677b59a358
cxpqwvtj <cxpqwvtj@gmail.com>
2016-10-21 06:50:50 +0900

    メインクラスを作ってテスト用に呼び出せるようにした

---
+++ buildSrc/src/main/groovy/app/himawari/Main.groovy
@@ -0,0 +1,12 @@
+package app.himawari
+
+import app.himawari.gen.ClassGenerator
+
+/**
+ * Created by masahiro on 2016/10/21.
+ */
+class Main {
+    static void main(String[] args) {
+        new ClassGenerator().generate()
+    }
+}
--- buildSrc/src/main/groovy/app/himawari/gen/ClassGenerator.groovy
+++ buildSrc/src/main/groovy/app/himawari/gen/ClassGenerator.groovy
@@ -1,10 +1,14 @@
 package app.himawari.gen
 
+import groovy.io.FileType
+
 /**
  * Created by masahiro on 2016/10/20.
  */
 class ClassGenerator {
     void generate() {
-        new File("").eachFileMatch()
+        new File("../sample").eachFileMatch(FileType.FILES, /.*\.xlsx/) {
+            println("${it.name}")
+        }
     }
 }
