74bb5893d512c9bc13c03d12bd5b7227a9f55f13
cxpqwvtj <cxpqwvtj@gmail.com>
2016-10-22 08:06:21 +0900

    設定ファイル読み込み処理実装中

--- buildSrc/src/main/groovy/app/himawari/gen/ClassGenerator.groovy
+++ buildSrc/src/main/groovy/app/himawari/gen/ClassGenerator.groovy
@@ -1,12 +1,18 @@
 package app.himawari.gen
 
 import groovy.io.FileType
+import org.apache.tools.ant.taskdefs.Classloader
 
 /**
  * Created by masahiro on 2016/10/20.
  */
 class ClassGenerator {
     void generate() {
+        def configFileUrl = new File("buildSrc/src/main/resources/config/default.config").toURI().toURL()
+        //def configFileUrl = ClassLoader.getSystemResource("config/default.config")
+        def defaultConfig = new ConfigSlurper().parse(configFileUrl)
+        println(defaultConfig)
+        def customConfig
         new File("../sample").eachFileMatch(FileType.FILES, /.*\.xlsx/) {
             println("${it.name}")
         }
