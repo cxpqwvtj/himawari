6b0c241fa4dc7ac15df8619baa95ac2019932ac1
cxpqwvtj <cxpqwvtj@gmail.com>
2016-11-05 09:49:41 +0900

    API表示用にルーティング追加

--- src/main/client/routes.js
+++ src/main/client/routes.js
@@ -5,11 +5,13 @@ import App from './containers/App'
 import PortalPage from './containers/PortalPage'
 import Login from './containers/Login'
 import TimeCard from './containers/TimeCard'
+import ApiSpec from './components/ApiSpec'
 
 export default (
   <Route path={`${process.env.CONTEXT_PATH}/`} component={App}>
     <IndexRoute component={PortalPage} />
     <Route path='login' component={Login} />
     <Route path='timecards(/:yearMonth)' component={TimeCard} />
+    <Route path='docs/api' component={ApiSpec} />
   </Route>
 )
