57b261c07718f3e04d7d50582c9d5296977a78c6
cxpqwvtj <cxpqwvtj@gmail.com>
2016-12-15 07:34:59 +0900

    生のJSONの形で出力できるようにした

--- buildSrc/src/main/javascript/index.js
+++ buildSrc/src/main/javascript/index.js
@@ -7,19 +7,29 @@ const buf = fs.readFileSync('./docs/schema/schema.json')
 // console.log(JSON.stringify({}, null, 2))
 const schemaDef = immutable.fromJS(parser.parse(JSON.parse(buf.toString())))
 
-const printRecursive = (obj, depth, parentKey) => {
-  obj.map((v, k) => {
-    if (parentKey === 'properties' && !(depth === 1 && k === 'error')) {
+const printRecursive = (obj, depth) => {
+  return obj.map((v, k) => {
+    if (k !== 'definitions' && !(depth === 1 && k === 'error')) {
       console.log(`[${depth}]${k} ${v.getIn(['type', 0])} ${v.get('description')}`)
+      if (v.getIn(['type', 0]).toLowerCase() === 'object') {
+        return immutable.fromJS({[k]: printRecursive(v.get('properties'), depth + 1)})
+      } else if (v.getIn(['type', 0]).toLowerCase() === 'array') {
+        const arrayProp = v.getIn(['items', 'properties'])
+        if (arrayProp) {
+          return immutable.fromJS({[k]: [printRecursive(arrayProp, depth + 1, k)]})
+        }
+        // TODO: arrayの中身がobjectではない場合を考慮する
+      } else if (v.getIn(['type', 0]).toLowerCase() === 'string') {
+        return immutable.fromJS({[k]: '文字列'})
+      } else if (v.getIn(['type', 0]).toLowerCase() === 'number') {
+        return immutable.fromJS({[k]: 0})
+      }
     }
-    if (k !== 'definitions' && typeof(v) === 'object') {
-      printRecursive(v, parentKey === 'properties' ? depth + 1 : depth, k)
-    }
-  })
+  }).reduce((r, v) => r.mergeDeep(v), immutable.fromJS({}))
 }
 
 // console.log(JSON.stringify(schemaDef.toJS(), null, 2))
 
-printRecursive(schemaDef, 1)
+console.log(JSON.stringify(printRecursive(schemaDef.get('properties'), 1).toJS(), null, 2))
 
 // console.log(JSON.stringify(schemaDef.get('properties').toJS(), null, 2))
