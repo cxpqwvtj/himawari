0d71301825bf4c72131a8ef25466981b1b615840
cxpqwvtj <cxpqwvtj@gmail.com>
2016-12-23 10:18:46 +0900

    dataクラス生成用の布石。文字列をコンソール出力

--- buildSrc/src/main/javascript/index.js
+++ buildSrc/src/main/javascript/index.js
@@ -27,27 +27,51 @@ const exampleJson = (obj, depth) => {
   }).reduce((r, v) => r.mergeDeep(v), immutable.fromJS({}))
 }
 
-const generateKotlinDataClass = (obj, depth) => {
+const generateVariable = (obj, depth) => {
+  const indent = immutable.Range(0, depth).map(() => '  ').reduce((r, v) => r + v, '')
   return obj.map((v, k) => {
     if (k !== 'definitions') {
-      const indent = immutable.Range(1, depth).map(() => '  ').reduce((r, v) => r + v, '')
-      console.log(`[${depth}]${k} ${v.getIn(['type', 0])} ${v.get('description')}`)
       if (v.getIn(['type', 0]).toLowerCase() === 'object') {
-        return `${indent}data class ${k[0].toUpperCase()}${k.substring(1)}\n`
+        return `${indent}var ${k}: ${k[0].toUpperCase()}${k.substring(1)}`
+      } else if (v.getIn(['type', 0]).toLowerCase() === 'array') {
+        const arrayProp = v.getIn(['items', 'properties'])
+        if (arrayProp) {
+          return `${indent}var ${k}: List<${k[0].toUpperCase()}${k.substring(1)}>`
+        }
+        // TODO: arrayの中身がobjectではない場合を考慮する
+      } else if (v.getIn(['type', 0]).toLowerCase() === 'string') {
+        return `${indent}var ${k}: String`
+      } else if (v.getIn(['type', 0]).toLowerCase() === 'number') {
+        return `${indent}var ${k}: Int`
+      }
+    }
+  }).map(v => `\n${v}`)
+  .join(',')
+}
+
+const generateKotlinDataClass = (obj, depth) => {
+  const indent = immutable.Range(0, depth).map(() => '  ').reduce((r, v) => r + v, '')
+  const classDefs = obj.map((v, k) => {
+    if (k !== 'definitions') {
+      // console.log(`[${depth}]${k} ${v.getIn(['type', 0])} ${v.get('description')}`)
+      if (v.getIn(['type', 0]).toLowerCase() === 'object') {
+        return `${indent}data class ${k[0].toUpperCase()}${k.substring(1)} (${generateVariable(immutable.fromJS(v.get('properties')), depth + 1)})${generateKotlinDataClass(immutable.fromJS(v.get('properties')), depth + 1)}`
         // return immutable.fromJS({[k]: generateKotlinDataClass(v.get('properties'), depth + 1)})
       } else if (v.getIn(['type', 0]).toLowerCase() === 'array') {
         const arrayProp = v.getIn(['items', 'properties'])
         if (arrayProp) {
-          return `List<${k[0].toUpperCase()}${k.substring(1)}> ${k}\n`
+          return `${indent}data class ${k[0].toUpperCase()}${k.substring(1)} (${generateVariable(immutable.fromJS(v.getIn(['items', 'properties'])), depth + 1)})${generateKotlinDataClass(immutable.fromJS(v.getIn(['items', 'properties'])), depth + 1)}`
         }
         // TODO: arrayの中身がobjectではない場合を考慮する
-      } else if (v.getIn(['type', 0]).toLowerCase() === 'string') {
-        return `var ${k}: String\n`
-      } else if (v.getIn(['type', 0]).toLowerCase() === 'number') {
-        return `var ${k}: Int\n`
       }
     }
-  }).reduce((r, v) => r += v)
+  }).filter(v => v)
+
+  if (classDefs.size == 0) {
+    return ''
+  } else {
+    return ` {\n${classDefs.join('\n')}\n${indent}}`
+  }
 }
 
 schemaDef.get('properties').map((v, k) => {
@@ -55,7 +79,7 @@ schemaDef.get('properties').map((v, k) => {
     const obj = exampleJson(immutable.fromJS(v.get('properties')), 1).toJS()
     console.log(`***** ${k} *****`)
     console.log(JSON.stringify(obj, null, 2))
-    console.log(generateKotlinDataClass(immutable.fromJS(v.get('properties')), 1))
+    console.log(`data class ${k}(${generateVariable(immutable.fromJS(v.get('properties')), 1)})${generateKotlinDataClass(immutable.fromJS(v.get('properties')), 1)}`)
     console.log('')
   }
 })
