f5d5ee717985a66a3fd0e59b268dba0b94700c53
cxpqwvtj <cxpqwvtj@gmail.com>
2016-12-23 12:51:41 +0900

    immutableの変数名を大文字始まりにした

--- buildSrc/src/main/javascript/index.js
+++ buildSrc/src/main/javascript/index.js
@@ -1,34 +1,34 @@
 const fs = require('fs')
 const util = require('util')
 const parser = require('json-schema-parser')
-const immutable = require('immutable')
+const Immutable = require('immutable')
 
 const buf = fs.readFileSync('./docs/schema/schema.json')
 // console.log(JSON.stringify({}, null, 2))
-const schemaDef = immutable.fromJS(parser.parse(JSON.parse(buf.toString())))
+const schemaDef = Immutable.fromJS(parser.parse(JSON.parse(buf.toString())))
 
 const exampleJson = (obj, depth) => {
   return obj.map((v, k) => {
     if (k !== 'definitions') {
       if (v.getIn(['type', 0]).toLowerCase() === 'object') {
-        return immutable.fromJS({[k]: exampleJson(v.get('properties'), depth + 1)})
+        return Immutable.fromJS({[k]: exampleJson(v.get('properties'), depth + 1)})
       } else if (v.getIn(['type', 0]).toLowerCase() === 'array') {
         const arrayProp = v.getIn(['items', 'properties'])
         if (arrayProp) {
-          return immutable.fromJS({[k]: [exampleJson(arrayProp, depth + 1, k)]})
+          return Immutable.fromJS({[k]: [exampleJson(arrayProp, depth + 1, k)]})
         }
         // TODO: arrayの中身がobjectではない場合を考慮する
       } else if (v.getIn(['type', 0]).toLowerCase() === 'string') {
-        return immutable.fromJS({[k]: '文字列'})
+        return Immutable.fromJS({[k]: '文字列'})
       } else if (v.getIn(['type', 0]).toLowerCase() === 'number') {
-        return immutable.fromJS({[k]: 0})
+        return Immutable.fromJS({[k]: 0})
       }
     }
-  }).reduce((r, v) => r.mergeDeep(v), immutable.fromJS({}))
+  }).reduce((r, v) => r.mergeDeep(v), Immutable.fromJS({}))
 }
 
 const generateVariable = (obj, depth) => {
-  const indent = immutable.Range(0, depth).map(() => '  ').reduce((r, v) => r + v, '')
+  const indent = Immutable.Range(0, depth).map(() => '  ').reduce((r, v) => r + v, '')
   return obj.map((v, k) => {
     if (k !== 'definitions') {
       if (v.getIn(['type', 0]).toLowerCase() === 'object') {
@@ -50,7 +50,7 @@ const generateVariable = (obj, depth) => {
 }
 
 const generateClassComment = (obj, depth) => {
-  const indent = immutable.Range(0, depth - 1).map(() => '  ').reduce((r, v) => r + v, '')
+  const indent = Immutable.Range(0, depth - 1).map(() => '  ').reduce((r, v) => r + v, '')
   const propertiesComment = obj.map((v, k) => {
     if (k !== 'definitions') {
       return `${indent} * @property ${k} ${v.get('description').replace('\n', '<br />')}`
@@ -60,13 +60,13 @@ const generateClassComment = (obj, depth) => {
 }
 
 const generateKotlinDataClass = (obj, depth) => {
-  const indent = immutable.Range(0, depth).map(() => '  ').reduce((r, v) => r + v, '')
+  const indent = Immutable.Range(0, depth).map(() => '  ').reduce((r, v) => r + v, '')
   const classDefs = obj.map((v, k) => {
     if (k !== 'definitions') {
       // console.log(`[${depth}]${k} ${v.getIn(['type', 0])} ${v.get('description')}`)
       if (v.getIn(['type', 0]).toLowerCase() === 'object') {
         return `${generateClassComment(v.get('properties'), depth + 1)}${indent}data class ${k[0].toUpperCase()}${k.substring(1)} (${generateVariable(v.get('properties'), depth + 1)})${generateKotlinDataClass(v.get('properties'), depth + 1)}`
-        // return immutable.fromJS({[k]: generateKotlinDataClass(v.get('properties'), depth + 1)})
+        // return Immutable.fromJS({[k]: generateKotlinDataClass(v.get('properties'), depth + 1)})
       } else if (v.getIn(['type', 0]).toLowerCase() === 'array') {
         const arrayProp = v.getIn(['items', 'properties'])
         if (arrayProp) {
