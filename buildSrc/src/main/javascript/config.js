1788994c6f8ce335707fe526634158f7b42256a5
cxpqwvtj <cxpqwvtj@gmail.com>
2016-12-24 10:36:14 +0900

    kotlinクラスファイルを出力する処理を実装

--- buildSrc/src/main/javascript/config.js
+++ buildSrc/src/main/javascript/config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  schemaJsonFilePath: './docs/schema/schema.json'
+  schemaJsonFilePath: './docs/schema/schema.json',
+  outputDir: './src/main/kotlin/app/himawari/dto/json/gen',
+  packageRootDir: 'src/main/kotlin/'
 }
