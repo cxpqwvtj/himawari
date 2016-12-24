96a984acfcb333341269eab3bd0c3c165d081f76
cxpqwvtj <cxpqwvtj@gmail.com>
2016-12-24 09:46:52 +0900

    Kotlinクラスの文字列構築処理リファクタ

--- buildSrc/src/main/javascript/index.js
+++ buildSrc/src/main/javascript/index.js
@@ -4,81 +4,60 @@ const parser = require('json-schema-parser')
 const Immutable = require('immutable')
 
 const buf = fs.readFileSync('./docs/schema/schema.json')
-// console.log(JSON.stringify({}, null, 2))
 const schemaDef = Immutable.fromJS(parser.parse(JSON.parse(buf.toString())))
 
 const exampleJson = (propertyName, jsonDef) => {
-  if (jsonDef.getIn(['type', 0]).toLowerCase() === 'object') {
-    const properties = jsonDef.get('properties') || Immutable.fromJS({})
+  if (jsonDef.getIn(['type', 0]).toLowerCase() === 'object' || jsonDef.getIn(['type', 0]).toLowerCase() === 'array') {
+    const properties = jsonDef.get('properties') || jsonDef.getIn(['items', 'properties'])
     return properties.map((v, k) => {
       return Immutable.fromJS({[k]: exampleJson(k, v)})
-    }).reduce((r, v) => r.mergeDeep(v))
-  } else if (jsonDef.getIn(['type', 0]).toLowerCase() === 'array') {
-    const properties = jsonDef.getIn(['items', 'properties']) || Immutable.fromJS({})
-    return properties.map((v, k) => {
-      return Immutable.fromJS([{[k]: exampleJson(k, v)}])
     }).reduce((r, v) => r.mergeDeep(v))
   } else if (jsonDef.getIn(['type', 0]).toLowerCase() === 'string') {
     return '文字列'
   } else if (jsonDef.getIn(['type', 0]).toLowerCase() === 'number') {
     return 0
+  } else {
+    console.log(`不明な型定義です。${jsonDef.get('type')}`) // eslint-disable-line no-console
   }
 }
 
-const generateVariable = (propertiesDef, depth) => {
-  const indent = Immutable.Range(0, depth).map(() => '  ').reduce((r, v) => r + v, '')
-  return propertiesDef.map((v, k) => {
-    if (k !== 'definitions') {
-      if (v.getIn(['type', 0]).toLowerCase() === 'object') {
-        return `${indent}var ${k}: ${k[0].toUpperCase()}${k.substring(1)}`
-      } else if (v.getIn(['type', 0]).toLowerCase() === 'array') {
-        const arrayProp = v.getIn(['items', 'properties'])
-        if (arrayProp) {
-          return `${indent}var ${k}: List<${k[0].toUpperCase()}${k.substring(1)}>`
-        }
-        // TODO: arrayの中身がobjectではない場合を考慮する
-      } else if (v.getIn(['type', 0]).toLowerCase() === 'string') {
-        return `${indent}var ${k}: String`
-      } else if (v.getIn(['type', 0]).toLowerCase() === 'number') {
-        return `${indent}var ${k}: Int`
-      }
+const generateVariable = (propertyName, jsonDef, depth) => {
+  const indent = Immutable.Range(0, depth).map(() => '  ').join('')
+  const properties = jsonDef.get('properties') || jsonDef.getIn(['items', 'properties']) || Immutable.fromJS({})
+  return properties.map((v, k) => {
+    if (v.getIn(['type', 0]).toLowerCase() === 'object') {
+      return `${indent}var ${k}: ${k[0].toUpperCase()}${k.substring(1)}`
+    } else if (v.getIn(['type', 0]).toLowerCase() === 'array') {
+      return `${indent}var ${k}: List<${k[0].toUpperCase()}${k.substring(1)}>`
+    } else if (v.getIn(['type', 0]).toLowerCase() === 'string') {
+      return `${indent}var ${k}: String`
+    } else if (v.getIn(['type', 0]).toLowerCase() === 'number') {
+      return `${indent}var ${k}: Int`
+    } else {
+      console.log(`不明な型定義です。${v.get('type')}`) // eslint-disable-line no-console
     }
-  }).map(v => `\n${v}`)
-  .join(',')
+  }).map(v => `\n${v}`).join(',')
 }
 
-const generateClassComment = (propertiesDef, depth) => {
+const generateClassComment = (proppropertyName, jsonDef, depth) => {
   const indent = Immutable.Range(0, depth - 1).map(() => '  ').reduce((r, v) => r + v, '')
-  const propertiesComment = propertiesDef.map((v, k) => {
-    if (k !== 'definitions') {
-      return `${indent} * @property ${k} ${v.get('description').replace('\n', '<br />')}`
-    }
-  }).join('\n')
-  return `${indent}/**\n${propertiesComment}\n${indent} */\n`
+  const properties = jsonDef.get('properties') || jsonDef.getIn(['items', 'properties']) || Immutable.fromJS({})
+  const propertiesComment = properties.map((v, k) => {
+    return `${indent} * @property ${k} ${v.get('description').replace('\n', '<br />')}`
+  }).map(v => `\n${v}`).join('')
+  return `${indent}/**\n${indent} * ${jsonDef.get('description').replace('\n', '<br />')}${propertiesComment}\n${indent} */\n`
 }
 
-const generateKotlinDataClass = (propertiesDef, depth) => {
+const generateKotlinDataClass = (propertyName, jsonDef, depth) => {
   const indent = Immutable.Range(0, depth).map(() => '  ').reduce((r, v) => r + v, '')
-  const classDefs = propertiesDef.map((v, k) => {
-    if (k !== 'definitions') {
-      // console.log(`[${depth}]${k} ${v.getIn(['type', 0])} ${v.get('description')}`)
-      if (v.getIn(['type', 0]).toLowerCase() === 'object') {
-        return `${generateClassComment(v.get('properties'), depth + 1)}${indent}data class ${k[0].toUpperCase()}${k.substring(1)} (${generateVariable(v.get('properties'), depth + 1)})${generateKotlinDataClass(v.get('properties'), depth + 1)}`
-        // return Immutable.fromJS({[k]: generateKotlinDataClass(v.get('properties'), depth + 1)})
-      } else if (v.getIn(['type', 0]).toLowerCase() === 'array') {
-        const arrayProp = v.getIn(['items', 'properties'])
-        if (arrayProp) {
-          return `${generateClassComment(v.getIn(['items', 'properties']), depth + 1)}${indent}data class ${k[0].toUpperCase()}${k.substring(1)} (${generateVariable(v.getIn(['items', 'properties']), depth + 1)})${generateKotlinDataClass(v.getIn(['items', 'properties']), depth + 1)}`
-        }
-        // TODO: arrayの中身がobjectではない場合を考慮する
-      }
-    }
-  }).filter(v => v)
-
-  if (classDefs.size == 0) {
-    return ''
-  } else {
-    return ` {\n${classDefs.join('\n')}\n${indent}}`
+  if (jsonDef.get('type').map(v => v.toLowerCase()).contains('object') ||
+    jsonDef.get('type').map(v => v.toLowerCase()).contains('array')) {
+    const properties = jsonDef.get('properties') || jsonDef.getIn(['items', 'properties'])
+    const children = properties.map((v, k) => {
+      return generateKotlinDataClass(k, v, depth + 1)
+    }).filter(v => v).map(v => `\n${v}`).join('')
+    const inner = children === '' ? '' : ` {${children}\n${indent}}`
+    return `${generateClassComment(propertyName, jsonDef, depth + 1)}${indent}data class ${propertyName[0].toUpperCase()}${propertyName.substring(1)}(${generateVariable(propertyName, jsonDef, depth + 2)})${inner}`
   }
 }
 
@@ -87,10 +66,7 @@ schemaDef.get('properties').map((v, k) => {
     console.log(`***** ${k} *****`)
     const obj = exampleJson(k, v, 1).toJS()
     console.log(JSON.stringify(obj, null, 2))
-    const classComment = generateClassComment(v.get('properties'), 1)
-    const variables = generateVariable(v.get('properties'), 1)
-    const dataClasses = generateKotlinDataClass(v.get('properties'), 1)
-    console.log(`${classComment}data class ${k}(${variables})${dataClasses}`)
+    console.log(generateKotlinDataClass(k, v, 0))
     console.log('')
   }
 })
