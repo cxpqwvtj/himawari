f03d9f3e588facb82f604f50416134883026cfcf
cxpqwvtj <cxpqwvtj@gmail.com>
2016-12-24 18:54:08 +0900

    サンプルJSONをファイル出力するように修正

--- buildSrc/src/main/javascript/config.js
+++ buildSrc/src/main/javascript/config.js
@@ -2,5 +2,6 @@ module.exports = {
   schemaJsonFilePath: './docs/schema/schema.json',
   mainPackageRoot: './src/main/kotlin',
   testPackageRoot: './src/test/kotlin',
-  packageName: 'app.himawari.dto.json.gen'
+  packageName: 'app.himawari.dto.json.gen',
+  sampleJsonDir: './src/test/resources/json/gen'
 }
