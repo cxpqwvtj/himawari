66dd48e922e24cc891ec6a97c28f2eefe979a6df
cxpqwvtj <cxpqwvtj@gmail.com>
2016-12-24 11:40:48 +0900

    出力先、パッケージ名の設定の仕方を変更

--- buildSrc/src/main/javascript/config.js
+++ buildSrc/src/main/javascript/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   schemaJsonFilePath: './docs/schema/schema.json',
-  outputDir: './src/main/kotlin/app/himawari/dto/json/gen',
-  packageRootDir: 'src/main/kotlin/'
+  mainPackageRoot: './src/main/kotlin',
+  testPackageRoot: './src/test/kotlin',
+  packageName: 'app.himawari.dto.json.gen'
 }
