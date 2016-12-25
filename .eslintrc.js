669a919e1beebc95f9e40b7859a2d29a84e70d16
cxpqwvtj <cxpqwvtj@gmail.com>
2016-12-25 10:48:44 +0900

    不要なダブルクォーテーションを削除。lintに従いシングルクォートに変更

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,35 @@
 module.exports = {
-  "env": {
-    "es6": true,
-    "node": true,
-    "browser": true
+  env: {
+    es6: true,
+    node: true,
+    browser: true
   },
-  "extends": ["eslint:recommended", "plugin:react/recommended"],
-  "parser": 'babel-eslint',
-  "parserOptions": {
-    "ecmaFeatures": {
-      "experimentalObjectRestSpread": true,
-      "jsx": true
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true
     },
-    "sourceType": "module"
+    sourceType: 'module'
   },
-  "plugins": [
-    "react"
+  plugins: [
+    'react'
   ],
-  "rules": {
-    "linebreak-style": [
-      "error",
-      "unix"
+  rules: {
+    'linebreak-style': [
+      'error',
+      'unix'
     ],
-    "quotes": [
-      "error",
-      "single"
+    quotes: [
+      'error',
+      'single'
     ],
-    "semi": [
+    semi: [
       2,
-      "never"
+      'never'
     ],
-    "no-constant-condition": [0],
-    "indent": ["error", 2]
+    'no-constant-condition': [0],
+    indent: ['error', 2]
   }
-};
+}
