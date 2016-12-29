fada6acfe160acf8c0d08c764a2999c8f9e37e8e
cxpqwvtj <cxpqwvtj@gmail.com>
2016-12-29 09:07:33 +0900

    エラーチェックを追加

--- buildSrc/src/main/javascript/ExcelReader.js
+++ buildSrc/src/main/javascript/ExcelReader.js
@@ -30,7 +30,15 @@ const fileExists = (filePath) => {
 
 const customConfigFilePath = './buildSrc/src/main/javascript/customConfig.json'
 const config = (fileExists(customConfigFilePath)) ? Immutable.fromJS(DefaultConfig).mergeDeep(Immutable.fromJS(JSON.parse(fs.readFileSync(customConfigFilePath).toString()))).toJS() : DefaultConfig
+if (!directoryExists(config.jsonDefXlsxDir)) {
+  console.log(`ディレクトリが存在しません。${config.jsonDefXlsxDir}`) // eslint-disable-line no-console
+  return
+}
 fs.readdir(config.jsonDefXlsxDir, (err, files) => {
+  if (err) {
+    console.log('ファイル検索でエラーが発生しました。', JSON.stringify(err, null, 2)) // eslint-disable-line no-console
+    return
+  }
   files.map((file) => {
     if (file.endsWith('.xlsx')) {
       console.log(`${config.jsonDefXlsxDir}/${file}`)
