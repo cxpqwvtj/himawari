e38caa3d0373690f7a36f993e0cbd9cdac37d321
cxpqwvtj <cxpqwvtj@gmail.com>
2017-01-01 08:11:14 +0900

    pathを使ったファイルパス解決に変更

--- buildSrc/src/main/javascript/config.js
+++ buildSrc/src/main/javascript/config.js
@@ -1,10 +1,11 @@
 const fs = require('fs')
 const JsYaml = require('js-yaml')
 const Immutable = require('immutable')
+const join = require('path').join;
 
 const Utils = require('./utils')
 
-const customFilePath = './buildSrc/src/main/javascript/custom-config.yml'
+const customFilePath = join(__dirname, '/custom_config.yml')
 
 const customConfig = Utils.existsFile(customFilePath) ? JsYaml.safeLoad(fs.readFileSync(customFilePath)) : {}
 
@@ -14,6 +15,6 @@ module.exports = Immutable.fromJS({
   testPackageRoot: './src/test/kotlin',
   packageName: 'app.himawari.dto.json.gen',
   sampleJsonDir: './src/test/resources/json/gen',
-  jsonDefXlsxDir: './buildSrc/src/main/resources/xlsx',
+  jsonDefXlsxDir: './docs/xlsx',
   outputHtmlDir: './docs/html'
 }).mergeDeep(customConfig).toJS()
