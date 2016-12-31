d06aafcd64106ec4cf35109dd4951d13b1049f85
cxpqwvtj <cxpqwvtj@gmail.com>
2017-01-01 08:14:04 +0900

    cellの値取得をloop内で直接処理するように修正

--- buildSrc/src/main/javascript/excelReader.js
+++ buildSrc/src/main/javascript/excelReader.js
@@ -1,31 +1,12 @@
 const fs = require('fs')
-const Excel = require('exceljs')
-const ejs = require('ejs')
-const yaml = require('js-yaml')
 const Immutable = require('immutable')
+const Excel = require('exceljs')
 const join = require('path').join;
+const yaml = require('js-yaml')
+const ejs = require('ejs')
 
 const Utils = require('./utils')
-const config = require('./config.js')
-
-const workbook = new Excel.Workbook()
-
-const cellValue = (cell) => {
-  if (cell.type === Excel.ValueType.Formula) {
-    //return cell.value.result
-  } else if (cell.type === Excel.ValueType.RichText) {
-    return cell.value.richText.map((v) => {
-      const bold = Immutable.fromJS(v).getIn(['font', 'bold']) ? 'font-weight: bold;' : ''
-      const strike = Immutable.fromJS(v).getIn(['font', 'strike']) ? 'text-decoration: line-through;' : ''
-      const italic = Immutable.fromJS(v).getIn(['font', 'italic']) ? 'font-style: italic;' : ''
-      const underline = Immutable.fromJS(v).getIn(['font', 'underline']) ? 'text-decoration: underline;' : ''
-      const color = Immutable.fromJS(v).getIn(['font', 'color', 'argb'])
-      // style='font-weight: bold; text-decoration: line-through; font-style: italic; text-decoration: underline; color: '
-      return `<span style='${bold}${strike}${italic}${underline}${color ? `color: #${color.substring(2)};` : ''}'>${v.text.replace(/\r\n/g, '\\n')}</span>`
-    }).join('')
-  }
-  return cell.toString().replace(/\r\n/g, '\\n')
-}
+const config = require('./config')
 
 if (!Utils.existsDirectory(config.jsonDefXlsxDir)) {
   console.log(`ディレクトリが存在しません。${config.jsonDefXlsxDir}`) // eslint-disable-line no-console
@@ -39,6 +20,7 @@ const promises = files.filter((file) => {
   return (books) => {
     return new Promise((resolve) => {
       console.log(`${config.jsonDefXlsxDir}/${file} ...`) // eslint-disable-line no-console
+      const workbook = new Excel.Workbook()
       workbook.xlsx.readFile(`${config.jsonDefXlsxDir}/${file}`).then(() => {
         const sheets = []
         workbook.eachSheet((worksheet) => {
@@ -47,7 +29,17 @@ const promises = files.filter((file) => {
           worksheet.eachRow((row, rowNumber) => {
             const cells = []
             row.eachCell({includeEmpty: true}, (cell, colNumber) => {
-              cells.push(cellValue(cell))
+              const value = (cell.type === Excel.ValueType.RichText) ? (
+                cell.value.richText.map((v) => {
+                  const bold = Immutable.fromJS(v).getIn(['font', 'bold']) ? 'font-weight: bold;' : ''
+                  const strike = Immutable.fromJS(v).getIn(['font', 'strike']) ? 'text-decoration: line-through;' : ''
+                  const italic = Immutable.fromJS(v).getIn(['font', 'italic']) ? 'font-style: italic;' : ''
+                  const underline = Immutable.fromJS(v).getIn(['font', 'underline']) ? 'text-decoration: underline;' : ''
+                  const color = Immutable.fromJS(v).getIn(['font', 'color', 'argb'])
+                  return `<span style='${bold}${strike}${italic}${underline}${color ? `color: #${color.substring(2)};` : ''}'>${v.text}</span>`
+                }).join('')
+              ) : cell.toString()
+              cells.push(value.replace(/\r\n/g, '<br/>'))
             })
             rows.push({cells})
           })
@@ -63,7 +55,7 @@ const promises = files.filter((file) => {
 promises.push((books) => {
   return new Promise(() => {
     fs.writeFileSync('./docs/excel/excel.yml', yaml.safeDump(books, {sortKeys: true}))
-    ejs.renderFile(join(__dirname, '/template/api-def.ejs'), {books}, {}, (err, html) => {
+    ejs.renderFile(join(__dirname, '/template/api_def.ejs'), {books}, {}, (err, html) => {
       if (err) {
         console.log(`HTMLのrender処理でエラーが発生しました。\n${err}`) // eslint-disable-line no-console
       } else {
