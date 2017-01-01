a1e54f4b86f43326dd22353b951a908bcb634cc1
cxpqwvtj <cxpqwvtj@gmail.com>
2017-01-01 17:26:24 +0900

    lintエラーを解決 タブからスペースに

--- src/main/client/actions/index.js
+++ src/main/client/actions/index.js
@@ -4,9 +4,9 @@ const FAILURE = 'FAILURE'
 
 function createRequestTypes(base) {
   return [REQUEST, SUCCESS, FAILURE].reduce((acc, type) => {
-		acc[type] = `${base}_${type}`
-		return acc
-	}, {})
+    acc[type] = `${base}_${type}`
+    return acc
+  }, {})
 }
 
 function action(type, payload = {}) {
