48d4665c2b8b2c4e9162cee21ad47fd46ce8cf9d
cxpqwvtj <cxpqwvtj@gmail.com>
2017-01-06 07:11:28 +0900

    不要なアノテーション定義を削除

--- src/main/kotlin/app/himawari/Application.kt
+++ src/main/kotlin/app/himawari/Application.kt
@@ -1,18 +1,12 @@
 package app.himawari
 
 import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Configuration
 
 /**
  * アプリケーションエントリーポイント
  * Created by masahiro on 2016/010/02.
  */
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan("app.himawari")
 @SpringBootApplication
 open class Application {
     companion object {
