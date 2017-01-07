4aad5043af4fbc60122fccc5169b3b184591c803
cxpqwvtj <cxpqwvtj@gmail.com>
2017-01-07 21:10:48 +0900

    DDL登録

---
+++ dbflute_himawari/playsql/replace-schema-00-system.sql
@@ -0,0 +1,9 @@
+-- #df:changeUser(system)#
+-- #df:checkUser(mainSchema)#
+create database /*$dfprop.mainCatalog*/;
+
+-- #df:reviveUser()#
+-- #df:checkUser(mainUser, grant)#
+grant all privileges on /*$dfprop.mainCatalog*/.* to /*$dfprop.mainUser*/@'%' identified by '/*$dfprop.mainPassword*/';
+
+flush privileges;
