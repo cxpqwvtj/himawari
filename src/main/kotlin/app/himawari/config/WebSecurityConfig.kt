64458c99415300722005b12e53a5d1cca03af7c2
cxpqwvtj <cxpqwvtj@gmail.com>
2017-01-19 07:00:48 +0900

    APIのセキュリティ設定と画面のセキュリティ設定を分けた

--- src/main/kotlin/app/himawari/config/WebSecurityConfig.kt
+++ src/main/kotlin/app/himawari/config/WebSecurityConfig.kt
@@ -1,5 +1,6 @@
 package app.himawari.config
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.security.SecurityProperties
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.annotation.Order
@@ -9,27 +10,42 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository
 
+
 /**
  * SpringSecurity用のJavaConfigクラスです。
  * Created by masahiro on 2016/11/12.
  */
-@Configuration
-@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
-open class WebSecurityConfig : WebSecurityConfigurerAdapter() {
-    override fun configure(web: WebSecurity) {
+class WebSecurityConfig {
+    @Autowired
+    fun configureGlobal(web: WebSecurity) {
         web.ignoring().mvcMatchers("/assets/**")
     }
 
-    override fun configure(httpSecurity: HttpSecurity) {
-        http.authorizeRequests().anyRequest().authenticated()
-                .and().formLogin()//.loginPage("/login").permitAll()
-                .and().logout().permitAll()
-        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-    }
-
-    override fun configure(auth: AuthenticationManagerBuilder) {
+    @Autowired
+    fun configureGlobal(auth: AuthenticationManagerBuilder) {
         auth.inMemoryAuthentication()
                 .withUser("admin").password("admin").roles("ADMIN", "USER")
                 .and().withUser("user").password("user").roles("USER")
+    }
+
+    @Configuration
+    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER - 1)
+    open class ApiWebSecurityConfigurationAdapter : WebSecurityConfigurerAdapter() {
+        @Throws(Exception::class)
+        override fun configure(http: HttpSecurity) {
+            http.antMatcher("/api/**").authorizeRequests().anyRequest().hasRole("ADMIN")
+                    .and().httpBasic()
+        }
+    }
+
+    @Configuration
+    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+    open class FormLoginWebSecurityConfigurerAdapter : WebSecurityConfigurerAdapter() {
+        override fun configure(httpSecurity: HttpSecurity) {
+            http.authorizeRequests().anyRequest().authenticated()
+                    .and().formLogin()//.loginPage("/login").permitAll()
+                    .and().logout().permitAll()
+            http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+        }
     }
 }
