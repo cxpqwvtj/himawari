3828cef2dd8f9f70b28504eaaac463ca97a80d57
cxpqwvtj <cxpqwvtj@gmail.com>
2017-01-22 05:19:41 +0900

    サーバ側ビルドスクリプト作成

--- dev-tools/build-on-docker.sh
+++ dev-tools/build-on-docker.sh
@@ -2,12 +2,20 @@
 
 $(dirname ${BASH_SOURCE:-$0})/project.sh
 
-IMAGE_NAME="cxpqwvtj/himawari-dev-client-image"
-
-IMAGES=`docker images | grep -c $IMAGE_NAME`
-
-if [ $IMAGES -lt 1 ]; then
-  docker build -f dev-tools/docker/dev-client-image/Dockerfile -t $IMAGE_NAME .
+# クライアントビルド
+CLIENT_DEV_IMAGE_NAME="cxpqwvtj/himawari-dev-client-image"
+CLIENT_DEV_IMAGES=`docker images | grep -c $CLIENT_DEV_IMAGE_NAME`
+if [ $CLIENT_DEV_IMAGES -lt 1 ]; then
+  docker build -f dev-tools/docker/dev-client-image/Dockerfile -t $CLIENT_DEV_IMAGE_NAME .
 fi
 
-docker run --rm -v `pwd`:/app $IMAGE_NAME
+#docker run --rm -v `pwd`:/app $CLIENT_DEV_IMAGE_NAME
+
+# サーバビルド
+SERVER_DEV_IMAGE_NAME="cxpqwvtj/himawari-dev-server-image"
+SERVER_DEV_IMAGES=`docker images | grep -c $SERVER_DEV_IMAGE_NAME`
+if [ $SERVER_DEV_IMAGES -lt 1 ]; then
+  docker build -f dev-tools/docker/dev-server-image/Dockerfile -t $SERVER_DEV_IMAGE_NAME .
+fi
+
+docker run --rm -v `pwd`:/app $SERVER_DEV_IMAGE_NAME
