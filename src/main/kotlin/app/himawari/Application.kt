f177df7dc795bd76852753989fae366f6504d3cb
cxpqwvtj <cxpqwvtj@gmail.com>
2017-02-11 17:56:50 +0900

    AspectJを依存関係に追加し、InterceptorでDBFluteのAccessContextを設定

--- src/main/kotlin/app/himawari/Application.kt
+++ src/main/kotlin/app/himawari/Application.kt
@@ -2,12 +2,14 @@ package app.himawari
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.EnableAspectJAutoProxy
 
 /**
  * アプリケーションエントリーポイント
  * Created by masahiro on 2016/010/02.
  */
 @SpringBootApplication
+@EnableAspectJAutoProxy
 open class Application {
     companion object {
         @JvmStatic fun main(args: Array<String>) {
---
+++ src/main/kotlin/app/himawari/interceptor/AccessContextInterceptor.kt
@@ -0,0 +1,58 @@
+package app.himawari.interceptor
+
+import app.himawari.model.BizDate
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
+import org.aspectj.lang.annotation.Aspect
+import org.dbflute.hook.AccessContext
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.stereotype.Component
+
+
+/**
+ * DBFluteのアクセスコンテキストを設定するためのインターセプタクラスです。
+ * Created by cxpqwvtj on 2017/02/11.
+ */
+@Component
+@Aspect
+class AccessContextInterceptor(
+        private val bizDate: BizDate
+) {
+
+    @Around("execution(* app.himawari.controller..*.*(..))")
+    fun around(point: ProceedingJoinPoint): Any {
+        if (AccessContext.isExistAccessContextOnThread()) {
+            // 既に設定されていたら何もしないで次へ
+            // (二度呼び出しされたときのために念のため)
+            return point.proceed()
+        }
+
+        // [アクセスユーザ]
+        // 例えば、セッション上のログインユーザを利用。
+        // ログインしていない場合のことも考慮すること。
+        val authentication = SecurityContextHolder.getContext().authentication
+        val accessUser = if (authentication == null) {
+            "anonymous"
+        } else {
+            val principal = authentication.principal
+            if (principal is UserDetails) {
+                principal.username
+            } else {
+                principal.toString()
+            }
+        }
+
+        val context = AccessContext()
+        context.accessLocalDateTime = bizDate.systemDate().toLocalDateTime()
+        context.accessUser = accessUser
+        AccessContext.setAccessContextOnThread(context)
+
+        try {
+            return point.proceed()
+        } finally {
+            // 最後はしっかりクリアすること (必須)
+            AccessContext.clearAccessContextOnThread()
+        }
+    }
+}
