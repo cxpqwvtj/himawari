2b67cddd0200799b86628d1c95dbddd623a54b66
cxpqwvtj <cxpqwvtj@gmail.com>
2017-02-16 06:10:12 +0900

    venderのjsは外出しに(調整中)

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ let CONTEXT_PATH = `${(process.env.CONTEXT_PATH || '')}`
 module.exports = {
   context: __dirname + '/src/main/client',
   entry: {
+    vender: ['react', 'redux', 'material-ui'],
     'js/bundle': [...(HOT_DEPLOY ? ['webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000&reload=true'] : []), './index.js']
   },
   output: {
@@ -25,6 +26,10 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env.CONTEXT_PATH': `"${CONTEXT_PATH}"`,
       'process.env.NODE_ENV': `"${process.env.NODE_ENV || (DEBUG ? 'development' : 'production')}"` 
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vender',
+      minChunks: Infinity
     }),
     ...(HOT_DEPLOY ? [new webpack.HotModuleReplacementPlugin()] : []),
     ...(DEBUG ? [] : [new webpack.optimize.AggressiveMergingPlugin(),
