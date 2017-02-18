7ac4ef1a7c2d68f1bf39c2cfb9c711b1efb2fbd1
cxpqwvtj <cxpqwvtj@gmail.com>
2017-02-18 16:58:22 +0900

    レスポンスのデータにない日もリスト表示するように修正

--- src/main/client/containers/TimeCard.js
+++ src/main/client/containers/TimeCard.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux'
 import Immutable from 'immutable'
 import moment from 'moment'
 
+import LinearProgress from 'material-ui/LinearProgress'
 import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar'
 import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } from 'material-ui/Table'
 import RaisedButton from 'material-ui/RaisedButton'
@@ -11,6 +12,7 @@ import RaisedButton from 'material-ui/RaisedButton'
 import AppBaseComponent from '../components/AppBaseComponent'
 
 import * as actions from '../actions'
+import { ROUTES } from '../constants'
 
 class TimeCard extends AppBaseComponent {
   static propTypes = {
@@ -27,8 +29,17 @@ class TimeCard extends AppBaseComponent {
     if (!this.props.timecard) {
       return <div></div>
     }
-    const rows = this.props.timecard.get('days', Immutable.fromJS([])).map((v, i) => {
-      const bizDate = moment(v.get('bizDate')).format('YYYY/MM/DD(ddd)')
+    const yearMonth = moment(this.props.timecard.get('yearMonth'), 'YYYYMM')
+    const res = this.props.timecard.get('days', Immutable.fromJS([])).reduce((r, v) => {
+      return r.merge({[v.get('bizDate')]: v})
+    }, Immutable.fromJS({}))
+    const days = Immutable.Range(0, yearMonth.clone().endOf('month').diff(yearMonth.clone().startOf('month'), 'days') + 1, 1).map(v => {
+      return yearMonth.clone().add(v, 'days')
+    }).map(v => {
+      return res.get(v.format('YYYY-MM-DD')) || Immutable.fromJS({bizDate: v.format('YYYY-MM-DD')})
+    })
+    const rows = days.map((v, i) => {
+      const bizDate = moment(v.get('bizDate')).format('MM/DD(ddd)')
       const start = v.get('startDatetime') ? moment(v.get('startDatetime')).format('HH:mm') : ''
       const end = v.get('endDatetime') ? moment(v.get('endDatetime')).format('HH:mm') : ''
       return (
@@ -42,15 +53,23 @@ class TimeCard extends AppBaseComponent {
       )
     })
     const buttonMargin = {margin: '10px'}
+    const prevMonth = yearMonth.clone().add(-1, 'months').format('YYYYMM')
+    const nextMonth = yearMonth.clone().add(1, 'months').format('YYYYMM')
     return (
       <div>
         <div>
-          <RaisedButton label='TOP' style={buttonMargin} onClick={() => {super.handleUrlChange('/')}} />
-          <RaisedButton label='テスト' style={buttonMargin} onClick={() => {this.props.actions.timecardLoadAction(this.props.params.yearMonth)}} />
+          <RaisedButton label='前月' style={buttonMargin} onClick={() => {
+            super.handleUrlChange(ROUTES.USER_TIMECARD(prevMonth))
+            this.props.actions.timecardLoadAction(prevMonth)
+          }} />
+          <RaisedButton label='次月' style={buttonMargin} onClick={() => {
+            super.handleUrlChange(ROUTES.USER_TIMECARD(nextMonth))
+            this.props.actions.timecardLoadAction(nextMonth)
+          }} />
         </div>
         <Toolbar>
           <ToolbarGroup>
-            <ToolbarTitle text="2017/01" />
+            <ToolbarTitle text={yearMonth.format('YYYY年MM月')} />
           </ToolbarGroup>
         </Toolbar>
         <div>
