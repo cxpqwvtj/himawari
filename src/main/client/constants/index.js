644ad2b836f7aaafff0c83b8effa6081e6d7e103
cxpqwvtj <cxpqwvtj@gmail.com>
2017-02-19 07:40:53 +0900

    記述順序変更

--- src/main/client/constants/index.js
+++ src/main/client/constants/index.js
@@ -1,11 +1,12 @@
-export const API_URL = {
-  JSON_SCHEMA: '/api/dev/docs/jsonschema',
-  USER_TIMECARD: (yearMonth) => `/api/v1/timecards/${yearMonth}`,
-}
 export const ROUTES = {
   USER_TIMECARD: (yearMonth) => `/timecards/${yearMonth}`,
   API_SPEC: '/dev/docs/api',
   MOCK_SETTING: '/dev/mock/setting'
+}
+
+export const API_URL = {
+  JSON_SCHEMA: '/api/dev/docs/jsonschema',
+  USER_TIMECARD: (yearMonth) => `/api/v1/timecards/${yearMonth}`,
 }
 
 export const HTTP_METHOD = {
