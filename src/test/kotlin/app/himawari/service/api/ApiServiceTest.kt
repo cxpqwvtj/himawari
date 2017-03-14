0c1883fd98da345a2f76adc6a109484d7074f43d
cxpqwvtj <cxpqwvtj@gmail.com>
2017-03-15 06:38:38 +0900

    AccessContext未設定でエラーになるテストコード記述

--- src/test/kotlin/app/himawari/service/api/ApiServiceTest.kt
+++ src/test/kotlin/app/himawari/service/api/ApiServiceTest.kt
@@ -2,6 +2,7 @@ package app.himawari.service.api
 
 import app.himawari.dto.json.Api0002Request
 import app.himawari.model.HimawariUser
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -21,12 +22,17 @@ internal class ApiServiceTest {
 
     @Test
     fun selectMonthlyInOutData() {
-        apiService.selectMonthlyInOutData("kougami", LocalDate.now())
+        val data = apiService.selectMonthlyInOutData("kougami", LocalDate.of(2017, 1, 1))
+        println(ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(data))
     }
 
     @Test
     fun createDailyStartEndHistory() {
-        apiService.createDailyStartEndHistory(HimawariUser(1, "kougami", "", listOf()), Api0002Request().apply { days = listOf() })
+        val dailyStartEndList = listOf(Api0002Request.Day().apply {
+            bizDate = "2017-03-15"
+            startDatetime = "2017-03-15T12:34:56+09:00"
+        })
+        apiService.createDailyStartEndHistory(HimawariUser(1, "kougami", "", listOf()), Api0002Request().apply { days = dailyStartEndList })
     }
 
 }
