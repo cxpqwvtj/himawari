34577c04f046bb799ce69de0e220a03375a1b604
cxpqwvtj <cxpqwvtj@gmail.com>
2017-03-17 07:26:08 +0900

    基底クラスをabstractに

--- src/test/kotlin/app/himawari/UnitHimawariContainerTestCase.kt
+++ src/test/kotlin/app/himawari/UnitHimawariContainerTestCase.kt
@@ -1,7 +1,10 @@
 package app.himawari
 
+import org.dbflute.utflute.spring.ContainerTestCase
+
 /**
+ * UT基底クラスです。
  * Created by cxpqwvtj on 2017/03/15.
  */
-class UnitHimawariContainerTestCase {
+abstract class UnitHimawariContainerTestCase : ContainerTestCase() {
 }
