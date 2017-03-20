4b3a33a927bbc466a339d7ac581c2764dcf2932e
cxpqwvtj <cxpqwvtj@gmail.com>
2017-03-21 06:40:33 +0900

    JUnit4なので、beforeとafterでsetupとtearDownを呼び出すように修正

--- src/test/kotlin/app/himawari/UnitHimawariContainerTestCase.kt
+++ src/test/kotlin/app/himawari/UnitHimawariContainerTestCase.kt
@@ -1,10 +1,21 @@
 package app.himawari
 
 import org.dbflute.utflute.spring.ContainerTestCase
+import org.junit.After
+import org.junit.Before
 
 /**
  * UT基底クラスです。
  * Created by cxpqwvtj on 2017/03/15.
  */
 abstract class UnitHimawariContainerTestCase : ContainerTestCase() {
+    @Before
+    fun before() {
+        super.setUp()
+    }
+
+    @After
+    fun after() {
+        super.tearDown()
+    }
 }
