88774813fc0d903a0422a3013d9beb4f65763e71
cxpqwvtj <cxpqwvtj@gmail.com>
2017-03-22 06:50:38 +0900

    swaggerでAPI定義するため、アプリ内での定義不要になり削除

--- src/main/client/routes.js
+++ src/main/client/routes.js
@@ -6,7 +6,6 @@ import PortalPage from './containers/PortalPage'
 import Login from './containers/Login'
 import TimeCardEntry from './containers/TimeCardEntry'
 import TimeCard from './containers/TimeCard'
-import ApiSpec from './containers/ApiSpec'
 import MockSetting from './containers/MockSetting'
 
 export default (
@@ -15,7 +14,6 @@ export default (
     <Route path='login' component={Login} />
     <Route path='timecards/entry(/:date)' component={TimeCardEntry} />
     <Route path='timecards(/:yearMonth)' component={TimeCard} />
-    <Route path='dev/docs/api' component={ApiSpec} />
     <Route path='dev/mock/setting' component={MockSetting} />
   </Route>
 )
