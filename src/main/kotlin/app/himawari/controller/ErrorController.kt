d419d5564b789e25c716ddee36f1148602539a22
cxpqwvtj <cxpqwvtj@gmail.com>
2017-03-30 07:02:26 +0900

    エラーページ用コントローラ作成

---
+++ src/main/kotlin/app/himawari/controller/ErrorController.kt
@@ -0,0 +1,11 @@
+package app.himawari.controller
+
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * サーバエラーが発生した場合のコントローラクラスです。
+ * Created by cxpqwvtj on 2017/03/30.
+ */
+@RestController
+open class ErrorController {
+}
