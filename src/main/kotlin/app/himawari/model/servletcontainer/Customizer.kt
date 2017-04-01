fdfabb90f52328f02eb738f3293410f77feafdeb
cxpqwvtj <cxpqwvtj@gmail.com>
2017-04-02 07:29:35 +0900

    404エラーページ設定

--- src/main/kotlin/app/himawari/model/servletcontainer/Customizer.kt
+++ src/main/kotlin/app/himawari/model/servletcontainer/Customizer.kt
@@ -2,13 +2,15 @@ package app.himawari.model.servletcontainer
 
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer
+import org.springframework.boot.web.servlet.ErrorPage
+import org.springframework.http.HttpStatus
 
 /**
  * サーブレットコンテナカスタムクラスです。
  * Created by cxpqwvtj on 2017/03/30.
  */
 class Customizer : EmbeddedServletContainerCustomizer {
-    override fun customize(container: ConfigurableEmbeddedServletContainer?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun customize(container: ConfigurableEmbeddedServletContainer) {
+        container.addErrorPages(ErrorPage(HttpStatus.NOT_FOUND, "/404"))
     }
 }
