e6df3d9a359edbafa98a3f0d8092855bd4a869a1
cxpqwvtj <cxpqwvtj@gmail.com>
2017-04-12 06:42:16 +0900

    アプリ用プロパティ定義

--- src/main/kotlin/app/himawari/model/AppProperty.kt
+++ src/main/kotlin/app/himawari/model/AppProperty.kt
@@ -16,4 +16,33 @@ class AppProperty {
         var timezone: String = "Asia/Tokyo"
         var bizDate: String = ""
     }
+
+    var log: Log = Log()
+
+    class Log {
+        var filter: Filter = Filter()
+
+        class Filter {
+            var type: String = "SIMPLE"
+        }
+    }
+
+    var timecard: TimeCard = TimeCard()
+
+    class TimeCard {
+        var excel: Excel = Excel()
+
+        class Excel {
+            val templateFilePath: String = "./template/timecard.xlsx"
+            val downloadFileName: String = "timecard.xlsx"
+            val outputSheetName: String = "timecard"
+            val beginRowNum: Int = 1
+            val bizDateColumnIndex: Int = 1
+            val startDatetimeColumnIndex: Int = 2
+            val endDatetimeColumnIndex: Int = 3
+            val vacationTypeColumnIndex: Int = 4
+            val noteColumnIndex: Int = 5
+            val autoSizeColumnIndexes: List<Int> = listOf(1, 2, 3, 4, 5)
+        }
+    }
 }
